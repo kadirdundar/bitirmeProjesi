b350012d03926049d3d4f891042371b9ad3cbe26
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-10-30 16:06:09 +0300

    giris sayfası ve harita eklentisi

--- bitirmeProject/ViewController.swift
+++ bitirmeProject/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sifreText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func girisYapTıklandı(_ sender: Any) {
+    }
+    
 
-
+    @IBAction func kayitolTiklandi(_ sender: Any) {
+    }
 }
 
--- bitirmeProject/choosingVC.swift
+++ bitirmeProject/choosingVC.swift
@@ -6,24 +6,23 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+class choosingVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
-class choosingVC: UIViewController {
-
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
