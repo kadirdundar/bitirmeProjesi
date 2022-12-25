360519bae41734dd0236ba967bd80ee4db9b1b2f
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-12-25 15:08:56 +0300

    güncelleme

--- bitirmeProject/savedVC.swift
+++ bitirmeProject/savedVC.swift
@@ -31,15 +31,15 @@ class savedVC: UIViewController {
     //let unscaledData = unscaleData.unscaleData(clusters: clusters, data: data)
     // bütün veriler çekilecek k-means ile hesapkayığ gönderilecek
     
-    func verileriAl(){
+    func verileriAl() {
         let firestoreDatabase = Firestore.firestore()
-        firestoreDatabase.collection("information").addSnapshotListener { [self] (snapshot, error) in
+        firestoreDatabase.collection("information").getDocuments { [self] (snapshot, error) in
             if error != nil{
                 print(error?.localizedDescription)
             }
             else {
                 if snapshot?.isEmpty != true && snapshot != nil {
-
+                    
                     for document in snapshot!.documents{
                         if let konum = document.get("location") as? GeoPoint{
                             self.locationData.append(konum)
@@ -52,12 +52,10 @@ class savedVC: UIViewController {
                     print(yenikonum[0])//basarılı
                     verileriIsle()
                 }
-                
             }
         }
-        
     }
-    
+
     func verileriIsle(){
         let scaledata = ScaleData(data: yenikonum).scaleData(data: yenikonum)
         let k = 5
@@ -65,54 +63,100 @@ class savedVC: UIViewController {
         let clusters = clusterer.cluster()
         
         let unscaleData = UnscaledData(clusters: clusters, data: yenikonum).unscaleData(clusters: clusters, data: yenikonum)
-      
-        //print(unscaleData)
-        func updateFirestore(aracnumarasi: Int, longitude: Double, latitude: Double, completion: @escaping (Error?) -> Void) {
-          let firestoreDatabase = Firestore.firestore()
-          let location = [GeoPoint(latitude: latitude, longitude: longitude)]
-          let geoPoint = location[0]
-          firestoreDatabase.collection("information").whereField("location", isEqualTo: geoPoint).getDocuments { [self] (snapshot, error) in
-            if let error = error {
-              completion(error)
-              return
-            } else {
-              for document in snapshot!.documents {
-                document.reference.updateData(["arac": aracnumarasi])
-              }
-            }
-            completion(nil)
-          }
-        }
-
+        
         var aracnumarasi = 1
-        for i in unscaleData {
-          for a in i {
-            let longitude = a[1]
-            let latitude = a[0]
-            updateFirestore(aracnumarasi: aracnumarasi, longitude: longitude, latitude: latitude) { error in
-              if let error = error {
-                // Handle the error here
-              }
+        var yenibirdizi = [[Double]: Int]()
+        for i in unscaleData{
+            for a in i {
+               
+                yenibirdizi[a] = aracnumarasi
             }
-          }
-          aracnumarasi = aracnumarasi + 1
+            aracnumarasi = aracnumarasi + 1
         }
+        print(yenibirdizi)
+        
+        //let firestoreDatabase = Firestore.firestore()
+        let semaphore = DispatchSemaphore(value: 0)
 
+        for (key, value) in yenibirdizi {
+            
+            
+            let longitude = key[1]
+            let latitude = key[0]
+            let point = [GeoPoint(latitude: latitude, longitude: longitude)]
+            let geoPoint = point[0]
+            let docRef = Firestore.firestore().collection("information").whereField("location", isEqualTo: geoPoint)
+            
+            docRef.getDocuments { (querySnapshot, error) in
+                  if let error = error {
+                     print(error.localizedDescription)
+                     return
+                  }
+                  guard let documents = querySnapshot?.documents else { return }
+                  for document in documents {
+                     document.reference.updateData(["arac": value])
+                      print(value)
+                  }
+               }
+            }
 
+        
+        //print(unscaleData)
+        /*let firestoreDatabase = Firestore.firestore()
+                
+        let queue = DispatchQueue(label: "updateQueue")
+        var aracnumarasi = 1
+        // Semaphore değişkeni oluşturulur
+        let semaphore = DispatchSemaphore(value: 0)
 
-
-           
-           
-           }
-           
-           
-           
-           
-
+        for i in unscaleData {
+            queue.async {
+                aracnumarasi += 1
+                for a in i {
+                    let longitude = a[1]
+                    let latitude = a[0]
+                    let point = [GeoPoint(latitude: latitude, longitude: longitude)]
+                    let geoPoint = point[0]
+                    print(geoPoint)
+                    
+                    firestoreDatabase.collection("information").whereField("location", isEqualTo: geoPoint).getDocuments { [self] (snapshot, error) in
+                        if let error = error {
+                            return
+                        } else {
+                            for document in snapshot!.documents {
+                                
+                                document.reference.updateData(["arac": aracnumarasi])
+                                print("burayagirdi")
+                            }
+                            semaphore.signal()
+                        }
+                        // Veri güncelleme işlemi tamamlandıktan sonra semaphore değişkeninin sayacı bir artırılır
+                        
+                    }
+                    // Semaphore değişkeni veri güncelleme işlemi tamamlandıktan önce çağrılıyor
+                    semaphore.wait()
+                }
+                
+            }
+        }
+        
+        // Tüm veri güncelleme işlemleri tamamlandıktan sonra bir sonraki adımın çalıştırılması gerekir
+        print("Veri güncelleme işlemleri tamamlandı.")
+        
+        */
     }
     
-    
-    //fonksiyon yazılacak konumu eşleşen verideki arac değişkeninin değerini değiştirecek
-
-
 }
+
+
+           
+           
+           
+           
+           
+           
+           
+
+    
+
+
