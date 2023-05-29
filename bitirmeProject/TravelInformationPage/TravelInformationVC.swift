364e160575b3d178dd4981ab30ad5f00bbad2aa6
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-05-29 13:58:42 +0300

    APIKeys.plist dosyasını .gitignore'a ekleme

---
+++ bitirmeProject/TravelInformationPage/TravelInformationVC.swift
@@ -0,0 +1,29 @@
+//
+//  TravelInformationVC.swift
+//  bitirmeProject
+//
+//  Created by Kadir Dündar on 24.05.2023.
+//
+
+import UIKit
+
+class TravelInformationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ bitirmeProject/driversPage/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  bitirmeProject
+//
+//  Created by Kadir Dündar on 24.05.2023.
+//
+
+import Foundation
